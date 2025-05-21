1a747284ebffafc71a2a867d6e9aa4819d08dca4
Axel Alvarade <axel.alvarade@hotmail.fr>
2025-05-21 16:43:14 +0200

    Finish

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,7 +5,7 @@ def index
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    if @restaurant.save!
+    if @restaurant.save
       redirect_to restaurant_path(@restaurant)
     else
       render "new", status: :unprocessable_entity
@@ -30,7 +30,7 @@ def index
   end
 
   private
-  
+
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
